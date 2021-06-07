cc3feb3728a865d6684d50f5730de9a5361ea379
Marek P <marek.bp@poczta.fm>
2021-06-07 13:26:41 +0200

    init

---
+++ script.js
@@ -0,0 +1,82 @@
+var button = document.getElementsByClassName("content")[0];
+var zoom = false;
+
+function sizeUp(){
+    if(zoom){
+        button.style.fontSize="18px";//wartość bez zooma
+        zoom = false;
+       }
+       else{
+        button.style.fontSize="25px";//wartość z zoomem
+        //zwiekszajaca sie mapka?
+        zoom = true;
+       }
+}
+
+
+let qwe = document.getElementById('mobileNav');
+let txt = document.getElementsByClassName('mobileLink');
+let state = false;
+
+function expandNav(){
+
+    if(state){
+        state = false;
+        qwe.style.animation = 'collapse 2s forwards';
+        for(let i = 0; i<3; i++){
+            txt[i].style.animation="fadeOut 0.5s forwards";
+        }
+    }
+    else{
+        state = true;
+        qwe.style.animation = 'expand 2s forwards';
+        for(let i = 0; i<3; i++){
+            txt[i].style.animation="fadeIn 1s 0.5s forwards";
+        }
+    }
+}
+
+window.addEventListener("resize", ()=>{
+
+    if(window.innerWidth>768){
+        qwe.style.animation = 'collapse 2s forwards';
+        for(let i = 0; i<3; i++){
+            txt[i].style.animation="fadeOut 0.5s forwards";
+        }
+    }
+
+});
+
+
+let i = 0;
+
+let hA0 = document.getElementById('bg1');
+let hA1 = document.getElementById('bg2');
+let hA2 = document.getElementById('bg3');
+hA0.style.animation = 'fadeIn 0.3s 1 forwards';
+hA1.style.animation = 'fadeOut 0.1s 1 forwards';
+hA2.style.animation = 'fadeOut 0.1s 1 forwards';
+
+setInterval(() => {
+
+    if(i==0){
+        hA0.style.animation = 'fadeOut 0.8s 1 forwards';
+        hA1.style.animation = 'fadeIn 1s 1 forwards';
+    }
+    else if(i==1){
+        hA1.style.animation = 'fadeOut 0.8s 1 forwards';
+        hA2.style.animation = 'fadeIn 1s 1 forwards';
+
+    }
+    else if(i==2){
+        hA2.style.animation = 'fadeOut 0.8s 1 forwards';
+        hA0.style.animation = 'fadeIn 1s 1 forwards';
+
+    }
+
+    i++
+    if(i==3){
+        i=0
+    }
+
+}, 3000);
